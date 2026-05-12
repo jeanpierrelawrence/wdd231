e9f424d1c3ef4df3a758308a8935536d86e1317b
Jp Lawrence <jeannpierrelawrence@gmail.com>
2026-05-12 21:36:49 +0200

    nav links updating after click

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -1,8 +1,20 @@
 const menuBtn = document.querySelector("#menu-button");
+const navLinks = document.querySelectorAll('.primary-nav ul li a');
 
 menuBtn.addEventListener("click", () => {
     document.body.classList.toggle("nav-open");
 
     const isOpen = document.body.classList.contains('nav-open');
-    menuBtn.setAttribute('aria-expanded', isOpen);
+    menuBtn.setAttribute("aria-expanded", isOpen);
 })
+
+navLinks.forEach(link => {
+    link.addEventListener("click", function() {
+
+        navLinks.forEach(l => l.classList.remove("active"));
+
+        this.classList.add("active");
+
+        document.body.classList.remove("nav-open");
+    });
+});
