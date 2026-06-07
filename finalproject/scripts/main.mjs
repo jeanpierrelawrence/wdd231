51254d51bfbdc029146d1d5ed6598dcbb03a9deb
Jp Lawrence <jeannpierrelawrence@gmail.com>
2026-06-07 18:12:38 +0200

    refactored javascript object to JSON and added new scenario data

--- finalproject/scripts/main.mjs
+++ finalproject/scripts/main.mjs
@@ -1,1 +1,18 @@
-import "./ui/modal.mjs";
+import { initHormoneModals } from "./ui/modal.mjs";
+
+async function initApp() {
+    try {
+        const response = await fetch("./data/data.json");
+        const data = await response.json();
+
+        if (data.hormones) {
+            initHormoneModals(data.hormones)
+        } else {
+            console.error("Hormones dictionary missing from JSON data structure.");
+        }
+    } catch (error) {
+        console.error("Failed to load application configuration data:", error);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", initApp);
--- finalproject/scripts/ui/modal.mjs
+++ finalproject/scripts/ui/modal.mjs
@@ -1,27 +1,28 @@
-import { hormones } from "../../data/hormones.mjs";
 import { displayModalInfo } from "./displayModalInfo.mjs";
 
 const dialog = document.querySelector("dialog");
 const pillPopups = document.querySelectorAll(".pill-popup");
 const closeModalBtn = document.querySelector(".modal-close");
 
-pillPopups.forEach(pill => {
-    pill.addEventListener("click", () => {
+export function initHormoneModals(hormones) {
+    pillPopups.forEach(pill => {
+        pill.addEventListener("click", () => {
 
-        const hormoneKey = pill.dataset.hormone;
-        const selectedHormone = hormones[hormoneKey];
+            const hormoneKey = pill.dataset.hormone;
+            const selectedHormone = hormones[hormoneKey];
 
-        if (selectedHormone) {
-            displayModalInfo(selectedHormone);
-            dialog.showModal();
-        } else {
-            console.warn(`Hormone data reference not found for key: "${hormoneKey}"`);
-        }
+            if (selectedHormone) {
+                displayModalInfo(selectedHormone);
+                dialog.showModal();
+            } else {
+                console.warn(`Hormone data reference not found for key: "${hormoneKey}"`);
+            }
+        })
+    });
+
+    closeModalBtn.addEventListener("click", () => {
+        dialog.close();
     })
-});
-
-closeModalBtn.addEventListener("click", () => {
-    dialog.close();
-})
+}
 
 
